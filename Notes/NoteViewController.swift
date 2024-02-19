e699cb77e7e120324bdadc1a4bb3573416ec3a39
sunshine <69354535+sunsh1nezxc@users.noreply.github.com>
2024-02-19 21:52:44 +0600

    notesAPP
    notesAPP

---
+++ Notes/NoteViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NoteViewController.swift
+//  Notes
+//
+//  Created by Amanzhol on 18.02.2024.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteLabel: UITextView!
+
+    public var noteTitle: String = ""
+    public var note: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = noteTitle
+        noteLabel.text = note
+    }
+
+}
